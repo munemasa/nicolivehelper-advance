842b6b2fb828ac7cc3b59385fc16a6103f19ad16
amano <amano@miku39.jp>
2014-02-11 22:45:38 +0900

    log???

--- chrome/content/mylistmanager/mylistmanager.js
+++ chrome/content/mylistmanager/mylistmanager.js
@@ -20,7 +20,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
  */
 function debugprint(str){
-    Application.console.log(str);
+    //Application.console.log(str);
 }
 
 
